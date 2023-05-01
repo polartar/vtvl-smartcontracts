d3b8deb22f2465597be6c246103b5e0b9a527f1f
polartar <polaris.center@outlook.com>
2023-05-01 08:05:42 -0700

    implement simple milestone contract

--- contracts/SimpleMilestone.sol
+++ contracts/SimpleMilestone.sol
@@ -7,21 +7,33 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SimpleMilestone is Ownable {
     bool public isCompleted;
     address public recipient;
-    address public tokenAddress;
+    IERC20 public tokenAddress;
+    bool public isDeposited;
+    uint256 public allocation;
 
     /**
     @notice Construct the contract.
     @param _tokenAddress - the address of the claim token.
+    @param _allocation - the allocation amount for this milestone.
     @param _recipient - the address for which we fetch the claim.
     @param _owner - the owner of this contract.
+    @param _isDeposited - the deposite state.
     @dev Factory contract will deposit the token when creating this contract.
     // This is created by Factory contract and Safe wallet can be used, 
     // so factory contract should pass address which will be the owner of this contract.
      */
-    constructor(address _tokenAddress, address _recipient, address _owner) {
+    constructor(
+        IERC20 _tokenAddress,
+        uint256 _allocation,
+        address _recipient,
+        address _owner,
+        bool _isDeposited
+    ) {
         recipient = _recipient;
         tokenAddress = _tokenAddress;
         _transferOwnership(_owner);
+        isDeposited = _isDeposited;
+        allocation = _allocation;
     }
 
     modifier onlyRecipient() {
@@ -36,20 +48,41 @@ contract SimpleMilestone is Ownable {
         _;
     }
 
-    /**
-    @notice Only recipient can withdraw when it's completed.
-    @dev Only onwer can mark as completed.
-     */
-    function setComplete() public onlyOwner {
-        isCompleted = true;
+    modifier onlyDeposited() {
+        uint256 balance = tokenAddress.balanceOf(address(this));
+        require(isDeposited || balance >= allocation, "NOT_DEPOSITED");
+
+        _;
     }
 
     /**
     @notice Only recipient can withdraw when it's completed.
+    @dev Only onwer can mark as completed.
+     */
+    function setComplete() public onlyOwner onlyDeposited {
+        isCompleted = true;
+    }
+
+    /**
+    @notice Only recipient can claim when it's completed.
     @dev Withdraw all tokens.
      */
-    function withdraw() public onlyRecipient onlyCompleted {
-        uint256 balance = IERC20(tokenAddress).balanceOf(address(this));
-        IERC20(tokenAddress).transfer(recipient, balance);
+    function claim() public onlyRecipient onlyCompleted {
+        IERC20(tokenAddress).transfer(recipient, allocation);
+    }
+
+    /**
+    @notice Only admin can withdraw the remaining balance.
+    @dev Withdraw the token amount that excludes allocation.
+     */
+    function withdraw() public onlyOwner {
+        uint256 balance = tokenAddress.balanceOf(address(this));
+        if (balance > allocation) {
+            IERC20(tokenAddress).transfer(msg.sender, balance - allocation);
+        }
+    }
+
+    function deposit(uint256 amount) public {
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
     }
 }
--- contracts/VTVLVestingFactory.sol
+++ contracts/VTVLVestingFactory.sol
@@ -1,30 +1,170 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.14;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./VTVLVesting.sol";
+import "./SimpleMilestone.sol";
+import "./VestingMilestone.sol";
 
 /// @title Vesting Factory contract
 /// @notice Create Vesting contract
 
 contract VTVLVestingFactory is Ownable {
-    event CreateVestingContract(
-        address indexed vestingAddress,
-        address deployer
+    event CreateMilestoneContract(
+        address indexed milestoneAddress,
+        uint256 milestoneIndex,
+        address creator
     );
 
     /**
-     * @notice Create Vesting contract
+    Check if sum of allocation percents is 100%
+     */
+    function allocationValidate(uint256[] _allocationPercents) private {
+        uint256 sum;
+        uint256 length = _allocationPercents.length;
+
+        for (uint256 i = 0; i < length; ) {
+            unchecked {
+                sum += _allocationPercents[i];
+                ++i;
+            }
+        }
+
+        if (sum != 10000) {
+            revert("INVALID_ALLOCATION_PERCENTS");
+        }
+    }
+
+    /**
+     * @notice Create milestone based Vesting contract
      * @param _tokenAddress Vesting Fund token address
      */
-    function createVestingContract(IERC20 _tokenAddress) public {
-        VTVLVesting vestingContract = new VTVLVesting(
-            _tokenAddress,
-            msg.sender
-        );
+    function createVestingContract(
+        IERC20 _tokenAddress,
+        uint256 _totalAllocation,
+        uint256 _allocationPercents,
+        address _recipient,
+        uint256 _intervalSecs,
+        uint256 _period
+    ) public {
+        allocationValidate(_allocationPercents);
 
-        emit CreateVestingContract(address(vestingContract), msg.sender);
+        uint256 _userBalance = _tokenAddress.balanceOf(msg.sender);
+        bool isDeposited;
+        if (_userBalance >= _tokenAddress) {
+            isDeposited = true;
+        }
+
+        _createVestingMilestones(
+            _tokenAddress,
+            _totalAllocation,
+            _allocationPercents,
+            _recipient,
+            _intervalSecs,
+            _period,
+            _isDeposited
+        );
+    }
+
+    function _createVestingMilestones(
+        IERC20 _tokenAddress,
+        uint256 _totalAllocation,
+        uint256[] _allocationPercents,
+        address _recipient,
+        uint256 _intervalSecs,
+        uint256 _period,
+        bool _isDeposited
+    ) private {
+        uint256 length = _allocationPercents.length;
+
+        for (uint256 i = 0; i < length; ) {
+            uint256 _allocation = (_totalAllocation * 10000) /
+                _allocationPercents[i];
+
+            VestingMilestone milestoneContract = new VestingMilestone(
+                _tokenAddress,
+                _allocation,
+                _recipient,
+                msg.sender,
+                _intervalSecs,
+                _period,
+                _isDeposited
+            );
+            if (_isDeposited) {
+                _tokenAddress.transferFrom(from, to, _allocation);
+            }
+
+            unchecked {
+                ++i;
+            }
+            emit CreateMilestoneContract(
+                address(milestoneContract),
+                i,
+                msg.sender
+            );
+        }
+    }
+
+    /**
+     * @notice Create simple milestones
+     * @param _tokenAddress Vesting Fund token address
+     */
+    function createSimpleMilestones(
+        IERC20 _tokenAddress,
+        uint256 _totalAllocation,
+        uint256 _allocationPercents,
+        address _recipient
+    ) public {
+        allocationValidate(_allocationPercents);
+
+        uint256 _userBalance = _tokenAddress.balanceOf(msg.sender);
+        bool isDeposited;
+        if (_userBalance >= _tokenAddress) {
+            isDeposited = true;
+        }
+
+        _createSimpleMilestones(
+            _tokenAddress,
+            _totalAllocation,
+            _allocationPercents,
+            _recipient,
+            _isDeposited
+        );
+    }
+
+    function _createSimpleMilestones(
+        IERC20 _tokenAddress,
+        uint256 _totalAllocation,
+        uint256[] _allocationPercents,
+        address _recipient,
+        bool _isDeposited
+    ) private {
+        uint256 length = _allocationPercents.length;
+
+        for (uint256 i = 0; i < length; ) {
+            uint256 _allocation = (_totalAllocation * 10000) /
+                _allocationPercents[i];
+
+            SimpleMilestone milestoneContract = new SimpleMilestone(
+                _tokenAddress,
+                _allocation,
+                _recipient,
+                msg.sender,
+                _isDeposited
+            );
+            if (_isDeposited) {
+                _tokenAddress.transferFrom(from, to, _allocation);
+            }
+
+            unchecked {
+                ++i;
+            }
+            emit CreateMilestoneContract(
+                address(milestoneContract),
+                i,
+                msg.sender
+            );
+        }
     }
 }
