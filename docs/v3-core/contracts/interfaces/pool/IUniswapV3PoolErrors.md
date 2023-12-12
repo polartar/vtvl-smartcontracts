c8ae2a8cde2addd41b70c8ebf780f3220a15c858
VTVL-admin <admin@vtvl.io>
2023-12-11 19:32:04 -0800

    feat: merge burnable feature

---
+++ docs/v3-core/contracts/interfaces/pool/IUniswapV3PoolErrors.md
@@ -0,0 +1,147 @@
+# IUniswapV3PoolErrors
+
+
+
+> Errors emitted by a pool
+
+Contains all events emitted by the pool
+
+
+
+
+
+## Errors
+
+### AI
+
+```solidity
+error AI()
+```
+
+
+
+
+
+
+### AS
+
+```solidity
+error AS()
+```
+
+
+
+
+
+
+### F0
+
+```solidity
+error F0()
+```
+
+
+
+
+
+
+### F1
+
+```solidity
+error F1()
+```
+
+
+
+
+
+
+### IIA
+
+```solidity
+error IIA()
+```
+
+
+
+
+
+
+### L
+
+```solidity
+error L()
+```
+
+
+
+
+
+
+### LOK
+
+```solidity
+error LOK()
+```
+
+
+
+
+
+
+### M0
+
+```solidity
+error M0()
+```
+
+
+
+
+
+
+### M1
+
+```solidity
+error M1()
+```
+
+
+
+
+
+
+### TLM
+
+```solidity
+error TLM()
+```
+
+
+
+
+
+
+### TLU
+
+```solidity
+error TLU()
+```
+
+
+
+
+
+
+### TUM
+
+```solidity
+error TUM()
+```
+
+
+
+
+
+
+
--- utils/constants.ts
+++ utils/constants.ts
@@ -5,16 +5,35 @@ export const INFURA_PROJECT_ID: string = process.env.INFURA_PROJECT_ID || "";
 
 export const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || "";
 
-export const SUPPORTED_CHAIN_NAMES = ["mainnet", "goerli", "bsc"];
+export const SUPPORTED_CHAIN_NAMES = [
+  "mainnet",
+  "goerli",
+  "base-goerli",
+  "satoshi",
+  "polygon",
+  "mumbai",
+  "avax",
+  "fuji",
+];
 
 export const SUPPORTED_RPC_ENDPOINTS = {
   mainnet: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
   goerli: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
-  bsc: `https://bsc.publicnode.com	`,
+  "base-goerli": "https://goerli.base.org",
+  satoshi: "https://rpc.satoshichain.io",
+  polygon: "https://polygon-rpc.com",
+  mumbai: "https://rpc-mumbai.maticvigil.com",
+  avax: "https://api.avax.network/ext/bc/C/rpc",
+  fuji: "https://avalanche-fuji-c-chain.publicnode.com",
 } as const;
 
 export const SUPPORTED_CHAIN_IDS = {
   mainnet: 1,
   goerli: 5,
-  bsc: 56,
+  "base-goerli": 84531,
+  satoshi: 5758,
+  mumbai: 80001,
+  polygon: 137,
+  avax: 43114,
+  fuji: 43113,
 } as const;
